3eb07cdf209898ef7904d5d09a50e170d1ff4588
GabrielaSaboia <gs525@njit.edu>
2020-02-12 12:29:43 -0500

    Starting javascript

--- landing.js
+++ landing.js
@@ -1,7 +1,7 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
 
-console.log(track);
+console.log(slides);
 //when I click left move slides to the left
 //when I click right, move slides to the right
 //when I click the nav indicators, move to that slide
