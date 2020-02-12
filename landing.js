742bf65ecdbdc040f8395278952a8116f16f6087
GabrielaSaboia <gs525@njit.edu>
2020-02-12 15:55:34 -0500

    Slides move all the way through

--- landing.js
+++ landing.js
@@ -1,7 +1,30 @@
 const track = document.querySelector('.carousel__track');
 const slides = Array.from(track.children);
+const nextButton = document.querySelector('.carousel__button--right');
+const prevButton = document.querySelector('.carousel__button--left');
+const dotsNav = document.querySelector('.carousel__nav');
+const dots = Array.from(dotsNav.children);
+const slideWidth = slides[0].getBoundingClientRect().width;
+//console.log(slideWidth);
 
-console.log(slides);
+
+//arrange the slides next to one another
+const setSlidePosition = (slide, index) =>{
+    slide.style.left = slideWidth * index + 'px';
+};
+slides.forEach(setSlidePosition);
+
 //when I click left move slides to the left
+nextButton.addEventListener('click', e => {
+    const currentSlide = track.querySelector('.current-slide');
+    const nextSlide = currentSlide.nextElementSibling;
+    const amountToMove = nextSlide.style.left;
+    //move to the next slide
+    track.style.transform = 'translateX(-' + amountToMove + ')';
+    currentSlide.classList.remove('current-slide');
+    nextSlide.classList.add('current-slide');
+});
 //when I click right, move slides to the right
+
 //when I click the nav indicators, move to that slide
+
