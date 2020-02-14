d238378297175049d00fe9fd56c32151a3e99960
GabrielaSaboia <gs525@njit.edu>
2020-02-14 12:31:48 -0500

    productList.js

---
+++ JS/productList.js
@@ -0,0 +1,13 @@
+//mock database for product list in JSON
+
+var mockDatabase = [
+    { _id: '123', name: 'Article 1', published: true },
+    { _id: '583', name: 'Article 2', published: true },
+    { _id: '954', name: 'Article 3', published: false },
+    { _id: '384', name: 'Article 4', published: false },
+    { _id: '183', name: 'Article 5', published: true },
+    { _id: '007', name: 'Article 6', published: false },
+    { _id: '304', name: 'Article 7', published: true },
+    { _id: '729', name: 'Article 8', published: false },
+    { _id: '734', name: 'Article 9', published: true },
+];
