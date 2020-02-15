26b1975c8d90d8761ceb3bfe4224eda418df3558
GabrielaSaboia <gs525@njit.edu>
2020-02-15 10:04:16 -0500

    Mock database with all data with the image source urls price and titles

--- JS/productList.js
+++ JS/productList.js
@@ -1,13 +1,14 @@
 //mock database for product list in JSON
 
 var database = [
-    { _id: '123', image: 'Article 1', title: true },
-    { _id: '583', name: 'Article 2', published: true },
-    { _id: '954', name: 'Article 3', published: false },
-    { _id: '384', name: 'Article 4', published: false },
-    { _id: '183', name: 'Article 5', published: true },
-    { _id: '007', name: 'Article 6', published: false },
-    { _id: '304', name: 'Article 7', published: true },
-    { _id: '729', name: 'Article 8', published: false },
-    { _id: '734', name: 'Article 9', published: true },
+    { _id: "1", price:"2,206,477", title: "Zakynthos", country: "Greece", image: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/your-shot-beach-scenes/016-your-shot-beaches-12380102_uploadsmember741708yourshot-741708-12380102jpg_l3jrr5qcuwugfq3di3pqcm6b5yzxs2udwatjavjxeftgooyp4jfa_5000x3611.jpg'},
+    { _id: "2", price:"1,927,469", title: "Snæfellsnes Peninsula", country: "Iceland", image: 'https://media.gadventures.com/media-server/cache/46/eb/46eb40ffabbded9bff671bee083188a8.png'},
+    { _id: "3", price:"1,583,509", title: "Diani Beach", country: "Kenya", image: "https://upload.wikimedia.org/wikipedia/commons/c/c7/Diani_Beach_Sunrise_Kenya.jpg"},
+    { _id: "4", price:"1,750,659", title: "Pig Beach", country: "The Bahamas", image: "https://bigseventravel.com/wp-content/uploads/2019/07/pig-island.jpg" },
+    { _id: "5", price:"1,470,902", title: "Playa Del Amor", country: "Mexico", image: "https://www.nationalgeographic.com/content/dam/travel/2017-digital/best-beaches/playa-del-amor-mexico.ngsversion.1529422222271.adapt.1900.1.jpg" },
+    { _id: "6", price:"990,999", title: "Source D'Argent", country: "Seychelles", image: "https://bigseventravel.com/wp-content/uploads/2019/07/source.jpeg" },
+    { _id: "7", price:"1,875,499", title: "Reynisfjara Beach", country: "Iceland", image: "https://bigseventravel.com/wp-content/uploads/2019/07/Grace-Bay-Turks-and-Caicos-1024x683.jpg" },
+    { _id: "8", price:"1,900,509", title: "Praia do Amor", country: "Brazil", image: "https://images.bestday.com/_lib/images/CONTENIDO-EDITORIAL/Blogs/Brasil/Miscelaneas/Melhores-Praias-para-Aproveitar-o-Verao/Praia-do-Amor-near-Pipa-Brazil.jpg" },
+    { _id: "9", price:"1,370,839", title: "Praia do Benagil", country: "Portugal", image: "https://mansion-global-app.s3.amazonaws.com/stories/portugal-special-report-portuguese-beach-report/media/alcalead_shorthand-mr.jpg" },
+    { _id: "10", price:"1,770,249", title: "Tayrona National Park", country: "Colombia", image: "https://theculturetrip.com/wp-content/uploads/2018/03/shutterstock_410041762.jpg" }
 ];
