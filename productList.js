7e48c0d9e6c150edd2455e22aa5a5eaba7480079
David Salazar <salazar.davidf@gmail.com>
2020-02-18 01:40:35 -0500

    feat: added three button for filter testing in productList.html and productList.js

---
+++ productList.js
@@ -0,0 +1,11 @@
+function button1(event){
+    alert('clicked button1');
+}
+
+function button2(event){
+    alert('clicked button2');
+}
+
+function button3(event){
+    alert('clicked button3');
+}
