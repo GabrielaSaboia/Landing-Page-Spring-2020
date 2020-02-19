33d31759574c2e4e1ae19103fb25fe399058964c
David Salazar <salazar.davidf@gmail.com>
2020-02-19 17:01:57 -0500

    feat: set up javascript array with two images

--- JS/productList.js
+++ JS/productList.js
@@ -1,8 +1,21 @@
 //mock database for product list in JSON
 
 var database = [
-    { _id: '123', image: 'Article 1', title: true },
-    { _id: '583', name: 'Article 2', published: true },
+    {
+        //_id: '123',
+        title: 'Zakynthos Greece Beach',
+        image: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/your-shot-beach-scenes/016-your-shot-beaches-12380102_uploadsmember741708yourshot-741708-12380102jpg_l3jrr5qcuwugfq3di3pqcm6b5yzxs2udwatjavjxeftgooyp4jfa_5000x3611.jpg',
+        location: 'Greece',
+        price: '900 mill',
+    },
+    {
+        title: 'Pig Beach',
+        image: 'https://bigseventravel.com/wp-content/uploads/2019/07/pig-island.jpg',
+        location: 'Bahamas',
+        price: '2 billion USD',
+
+    },
+    /**
     { _id: '954', name: 'Article 3', published: false },
     { _id: '384', name: 'Article 4', published: false },
     { _id: '183', name: 'Article 5', published: true },
@@ -10,4 +23,5 @@ var database = [
     { _id: '304', name: 'Article 7', published: true },
     { _id: '729', name: 'Article 8', published: false },
     { _id: '734', name: 'Article 9', published: true },
-];
+     ***/
+    ];
