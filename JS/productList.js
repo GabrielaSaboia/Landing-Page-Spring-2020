0c16e62eb3bb578d586c50c9600f88fe94c8cfb7
David Salazar <salazar.davidf@gmail.com>
2020-02-19 17:06:04 -0500

    feat: wrapping up javascript function for product page

--- JS/productList.js
+++ JS/productList.js
@@ -1,27 +1,26 @@
 //mock database for product list in JSON
+(function() {
 
-var database = [
-    {
-        //_id: '123',
-        title: 'Zakynthos Greece Beach',
-        image: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/your-shot-beach-scenes/016-your-shot-beaches-12380102_uploadsmember741708yourshot-741708-12380102jpg_l3jrr5qcuwugfq3di3pqcm6b5yzxs2udwatjavjxeftgooyp4jfa_5000x3611.jpg',
-        location: 'Greece',
-        price: '900 mill',
-    },
-    {
-        title: 'Pig Beach',
-        image: 'https://bigseventravel.com/wp-content/uploads/2019/07/pig-island.jpg',
-        location: 'Bahamas',
-        price: '2 billion USD',
+    var database = [
+        {
+            //_id: '123',
+            title: 'Zakynthos Greece Beach',
+            image: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/your-shot-beach-scenes/016-your-shot-beaches-12380102_uploadsmember741708yourshot-741708-12380102jpg_l3jrr5qcuwugfq3di3pqcm6b5yzxs2udwatjavjxeftgooyp4jfa_5000x3611.jpg',
+            location: 'Greece',
+            price: '900 mill',
+        },
+        {
+            title: 'Pig Beach',
+            image: 'https://bigseventravel.com/wp-content/uploads/2019/07/pig-island.jpg',
+            location: 'Bahamas',
+            price: '2 billion USD',
 
-    },
-    /**
-    { _id: '954', name: 'Article 3', published: false },
-    { _id: '384', name: 'Article 4', published: false },
-    { _id: '183', name: 'Article 5', published: true },
-    { _id: '007', name: 'Article 6', published: false },
-    { _id: '304', name: 'Article 7', published: true },
-    { _id: '729', name: 'Article 8', published: false },
-    { _id: '734', name: 'Article 9', published: true },
-     ***/
+        },
     ];
+
+    function renderList(results){
+
+
+    }
+})(); //wrapping entire file with locally exec function
+      //variable scope: local scope
