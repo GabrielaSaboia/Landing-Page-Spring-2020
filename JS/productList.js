4f940d489de8cbeac8d8d59f3e2c9055e78b564c
David Salazar <salazar.davidf@gmail.com>
2020-02-20 06:34:56 -0500

    feat: adding css for beach cards

--- JS/productList.js
+++ JS/productList.js
@@ -101,7 +101,7 @@
                             <div class ="col-12">
                                 <div class = "card-deck">
                                    
-                                        <div class="card mb-3">
+                                        <div class="card mb-3" style = "min-width: 20rem;">
                                           
                                  
                                              
