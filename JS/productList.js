8d0d629556b62a1595ad4da95db2c9186f2372d2
David Salazar <salazar.davidf@gmail.com>
2020-02-20 01:37:08 -0500

    update: set up the js array for all carousel pictures to be displayed in second page

--- JS/productList.js
+++ JS/productList.js
@@ -27,14 +27,62 @@
             name: 'Zakynthos Greece Beach',
             image: 'https://www.nationalgeographic.com/content/dam/travel/2019-digital/your-shot-beach-scenes/016-your-shot-beaches-12380102_uploadsmember741708yourshot-741708-12380102jpg_l3jrr5qcuwugfq3di3pqcm6b5yzxs2udwatjavjxeftgooyp4jfa_5000x3611.jpg',
             location: 'Greece',
-            price: '900 mill',
+            price: '900',
+        },
+        {
+            name: 'Snæfellsnes Peninsula Iceland',
+            image: 'https://media.gadventures.com/media-server/cache/46/eb/46eb40ffabbded9bff671bee083188a8.png',
+            location: 'Iceland',
+            price: '100',
+        },
+        {
+            name:   'Diani Beach',
+            image:  'https://upload.wikimedia.org/wikipedia/commons/c/c7/Diani_Beach_Sunrise_Kenya.jpg',
+            location: 'Kenya',
+            price:  '800',
         },
         {
             name: 'Pig Beach',
             image: 'https://bigseventravel.com/wp-content/uploads/2019/07/pig-island.jpg',
             location: 'Bahamas',
-            price: '2 billion USD',
+            price: '300',
 
+        },
+        {
+            name: 'Playa Del Amor',
+            image: 'https://www.nationalgeographic.com/content/dam/travel/2017-digital/best-beaches/playa-del-amor-mexico.ngsversion.1529422222271.adapt.1900.1.jpg',
+            location: 'Mexico',
+            price: '200',
+        },
+        {
+            name: 'Source D\'Argent Seychelles',
+            image: 'https://bigseventravel.com/wp-content/uploads/2019/07/source.jpeg',
+            location: 'Seychelles',
+            price: '400',
+        },
+        {
+            name: 'Reynisfjara Beach Iceland',
+            image: 'https://bigseventravel.com/wp-content/uploads/2019/07/Grace-Bay-Turks-and-Caicos-1024x683.jpg',
+            location: 'Iceland',
+            price: '50',
+        },
+        {
+            name: 'Praia do Amor Brazil',
+            image: 'https://images.bestday.com/_lib/images/CONTENIDO-EDITORIAL/Blogs/Brasil/Miscelaneas/Melhores-Praias-para-Aproveitar-o-Verao/Praia-do-Amor-near-Pipa-Brazil.jpg',
+            location: 'Brazil',
+            price: '1000'
+        },
+        {
+            name: 'Praia do Benagil Portugal',
+            image: 'https://mansion-global-app.s3.amazonaws.com/stories/portugal-special-report-portuguese-beach-report/media/alcalead_shorthand-mr.jpg',
+            location: 'Portugal',
+            price: '900',
+        },
+        {
+            name: 'Tayrona National Park, Colombia',
+            image: 'https://theculturetrip.com/wp-content/uploads/2018/03/shutterstock_410041762.jpg',
+            location: 'Colombia',
+            price: '10000',
         },
     ];
 
@@ -51,7 +99,7 @@
                 </div>
                 `)
         }).reduce((a, b) => a.concat(b), "")    //a (empty string) same thing consistently. b is new thing. b element in list.
-    }                                           //for loop under hood updating empty string a with an element b 
+    }                                           //for loop under hood updating empty string a with an element b
 
     render(database);
 })() //wrapping entire file with locally exec function
