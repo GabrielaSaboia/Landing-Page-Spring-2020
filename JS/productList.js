4a7c5087f14188b2ea70011b0bd6f2b385a731e6
David Salazar <salazar.davidf@gmail.com>
2020-02-20 04:15:02 -0500

    feat: working with cards

--- JS/productList.js
+++ JS/productList.js
@@ -99,13 +99,13 @@
             return(`
                     <div class = "card-deck">
                         <div class = "col-sm-4">
-                        <div class = "card">    
-                            <img class ="card-img-top" src = "${beach.image}" alt="Card image cap">
-                            <div class ="card-body">
-                                <h5 class = "card-title">"${beach.name}"</h5>
-                                <p class = "card-text">"${beach.price}"</p>
-                             </div>
-                        </div>
+                            <div class = "card">    
+                                <img class ="card-img-top" src = "${beach.image}" alt="Card image cap">
+                                <div class ="card-body">
+                                    <h5 class = "card-title">"${beach.name}"</h5>
+                                    <p class = "card-text">"${beach.price}"</p>
+                                 </div>
+                            </div>
                         </div>
                     </div>
                 `)
