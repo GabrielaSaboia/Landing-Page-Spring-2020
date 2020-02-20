1a52fe6cacec41d6456fb8292b18d90a387da3b0
David Salazar <salazar.davidf@gmail.com>
2020-02-20 03:54:19 -0500

    feat: working with cards

--- JS/productList.js
+++ JS/productList.js
@@ -97,7 +97,7 @@
 
         cardContainer.innerHTML = results.map(function (beach,index){
             return(`
-                    <div class = "container">
+                    <div class = "card-columns">
                         <div class = "row">
                             <div class = "col-3">
                                 <div class = "card-group">
