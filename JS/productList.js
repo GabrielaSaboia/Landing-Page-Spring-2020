27e52aaacd96ca7cc2f204129e080477566231b4
David Salazar <salazar.davidf@gmail.com>
2020-02-24 05:05:58 -0500

    feat: working on filters

--- JS/productList.js
+++ JS/productList.js
@@ -99,7 +99,7 @@
     render(database);
 
 
-
+    /***
     function orderByInhabitants(inhabited) {
         if (inhabited === "true") {
             var sortedResults = (inhabited === 'true');
@@ -123,6 +123,7 @@
             render(filteredresults);
         }
     }
+     ****/
 
     document.querySelector('#inhabited').addEventListener('change', function(event){
         //var value = event.target.value === 'true';
@@ -173,16 +174,52 @@
                         return 1;
                     }
                 }) :
-
                 database.sort(function(a,b){
                     return a[price] - b[price];
                 });
             render(sortedPrices)
         }
-
     }
-
     document.querySelector('#price').addEventListener('change', function (event) {
         orderByPrice(event.target.value);
     });
+
+    function orderByInhabitants(inhabited) {
+        var a = new Array(10);
+        ///if (inhabited === "true") {
+            var sortedResults = (inhabited === 'true')?
+                database.filter(function (result) {
+                    var inhab1 = result.inhabited;
+
+                    if (sortedResults === inhab1)
+                        return result;
+                });
+                database.filter(function (b){
+
+                }
+    }
+
+
+
+/***
+
+                    if (sortedResults === result.inhabited)
+                //return sortedResults;
+                    return result.inhabited;
+            });
+            render(filteredresults);
+        } else if (inhabited === "false") {
+            var sortedResults = (inhabited === 'false');            //trying to convert to boolean false
+            var filteredresults = database.filter(function (result) {
+                if (sortedResults === result.inhabited)
+                    return result.inhabited;
+            });
+            render(filteredresults);
+        }
+    }
+***/
+
+
+
+
 })();
