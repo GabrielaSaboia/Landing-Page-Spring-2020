dc7665c032028f9b5593a9d58080806624527fcd
David Salazar <salazar.davidf@gmail.com>
2020-02-23 23:45:06 -0500

    feat: mergin davebranc3 into main

--- JS/productList.js
+++ JS/productList.js
@@ -266,7 +266,7 @@
                 return showInhabited;
             }
             else{
-                return result.inhabited;
+                return result.inhabited = false; 
             }
         });
         render(filteredResults);
